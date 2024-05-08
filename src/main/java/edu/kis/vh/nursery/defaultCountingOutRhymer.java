8824196e8ac0926f208f807395ebb726525dc633
Jakub Bak <245762@edu.p.lodz.pl>
2024-05-08 17:16:44 +0200

    sds

--- src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
@@ -2,33 +2,37 @@ package edu.kis.vh.nursery;
 
 public class defaultCountingOutRhymer {
 
-    private int[] NUMBERS = new int[12];
+    public static final int num12 = 12;
+    public static final int numMinus1 = -1;
+    public static final int num11 = 11;
+    private final int[] NUMBERS = new int[num12];
 
-    public int total = -1;
+    public int total = numMinus1;
 
     public void countIn(int in) {
         if (!isFull())
             NUMBERS[++total] = in;
     }
 
-        public boolean callCheck() {
-            return total == -1;
-        }
-        
-            public boolean isFull() {
-                return total == 11;
-            }
-        
-                protected int peekaboo() {
-                    if (callCheck())
-                        return -1;
-                    return NUMBERS[total];
-                }
-            
-                    public int countOut() {
-                        if (callCheck())
-                            return -1;
-                        return NUMBERS[total--];
-                    }
+    public boolean callCheck() {
+        return total == numMinus1;
+    }
+
+    public boolean isFull() {
+        return total == num11;
+    }
+
+    protected int peekaboo() {
+        if (callCheck())
+            return numMinus1;
+        return NUMBERS[total];
+    }
+
+
+    public int countOut() {
+        if (callCheck())
+            return numMinus1;
+        return NUMBERS[total--];
+    }
 
 }
